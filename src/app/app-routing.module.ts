50df19bc8038e5de678de57aa86b164390304151
Lucas <lucashenriquedml@gmail.com>
2024-08-10 14:24:49 -0300

    Inicializando o projeto Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'child1', loadChildren: () => import('./modulo1/modulo1.module').then(m => m.Modulo1Module) }, { path: 'child2', loadChildren: () => import('./modulo2/modulo2.module').then(m => m.Modulo2Module) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
